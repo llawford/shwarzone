e68e816296a7a8a0fe063e268ed3cca8386bc78a
Dave Pagurek <dave@davepagurek.com>
2018-07-14 14:47:08 -0400

    Add skip button

--- src/game.js
+++ src/game.js
@@ -11,6 +11,7 @@ const upgrade = document.getElementById('upgrade');
 const hire = document.getElementById('hire');
 const fire = document.getElementById('fire');
 const menu = document.getElementById('menu');
+const skip = document.getElementById('skip');
 
 const hideMenu = () => document.body.removeChild(menu);
 const showMenu = () => document.body.appendChild(menu);
@@ -22,8 +23,8 @@ window.preload = () => {
 }
 
 window.setup = () => {
-    createCanvas($(window).width() / 30*30, 30*25).parent(document.getElementById('game'));
-    map = new Map(upgrade, hire, fire, showMenu, hideMenu);
+    createCanvas(30*25, 30*20).parent(document.getElementById('game'));
+    map = new Map(upgrade, hire, fire, skip, showMenu, hideMenu);
 };
 
 window.draw = () => {
