9967c5200ef2b1d87b7d91da6a1ffeaae32a2e1d
Dave Pagurek <dave@davepagurek.com>
2018-07-14 09:32:05 -0400

    Add p5

--- src/game.js
+++ src/game.js
@@ -1,1 +1,17 @@
-alert("Hello, world");
+import * as p5 from 'p5';
+
+const setup = (sketch) => {
+  sketch.setup = () => {
+    sketch
+      .createCanvas(800, 600)
+      .parent(document.getElementById('game'));
+
+    sketch.background(40);
+  };
+
+  sketch.draw = () => {
+    // TODO
+  };
+};
+
+new p5(setup);
